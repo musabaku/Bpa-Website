f645cceed1fc014d917798ca20f878cc1e88c95b
Musab <musabkhan2003@gmail.com>
2023-08-11 21:51:07 +0300

    d

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,10 @@ const path = require("path");
     next();
   });
   
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../../frontend/build")));
   
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
   });
   
   
